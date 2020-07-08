cde03ec1f5a4eeebfa1abfc0282c7fd9570edb1d
Frandy Francisco Mota Rodriguez <frandy_mota@ucne.edu.do>
2020-07-08 11:13:18 -0700

    Add project files.

---
+++ Parcial2-AP2-Frandy/DAL/Contexto.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial2_AP2_Frandy.DAL
+{
+    public class Contexto : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+
+
+        }
+    }
+}
